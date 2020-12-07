21d2491829f6796c42bbecf88d028042029ac955
yuskey9 <yuskey0130@gmail.com>
2020-12-07 17:52:42 +0900

    新規登録・ログイン共にエラーハンドリング実装完了

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,22 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+
+
   validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
-  validates :birth_date, presence: true
+
+  password_regex = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: password_regex, message: 'please set including both letters and numbers.' 
+
+  with_options presence: true do
+    validates :last_name, :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "It is invalid. Please enter the user's real name in full-width." }   
+  end
+
+  with_options presence: true do
+    validates :last_name_kana, :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "It is invalid. Please enter the user's real name in full-width katakana." } 
+  end  
+
+  validates :birth_date, presence: true  
 
   has_many :items
   has_many :orders
