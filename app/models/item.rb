51a5b84ee37c43ca70c458a74a0f567ab6e96b29
yuskey9 <yuskey0130@gmail.com>
2020-12-09 11:28:50 +0900

    Active Storageの導入完了

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,8 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :name, :info, :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, :price
   end
-  
+
   belongs_to :user
   has_one :order
+  has_one_attached :image
 end
