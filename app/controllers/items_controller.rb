538799c07c0c89e1d6b21ee0271c6a45a1c06fcd
yuskey9 <yuskey0130@gmail.com>
2020-12-09 20:33:26 +0900

    コントローラーでの商品一覧ページと商品出品ページのルーティングの実装完了

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,8 +6,23 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
   
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to items_path
+    else
+      render :new
+    end  
+  end  
   
 
+end
+
+private
+
+def item_params
+  params.require(:item).permit(:name, :info, :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, :price).merge(user_id: current_user.id)
 end
