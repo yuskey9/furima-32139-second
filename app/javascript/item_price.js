1670b8baaa4375029a265915daaa4a541fa3a70e
yuskey9 <yuskey0130@gmail.com>
2020-12-12 21:43:16 +0900

    コードレビューによる修正完了

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -3,16 +3,12 @@ window.addEventListener('load', () => {
   const priceInput = document.getElementById("item-price");
   priceInput.addEventListener("input", () => {
     const inputValue = priceInput.value;
-    console.log(inputValue);
 　　
   const addTaxDom = document.getElementById("add-tax-price");
   addTaxDom.innerHTML = (Math.floor(inputValue * 0.1));
-   console.log(addTaxDom);
 
   const salesProfit = document.getElementById("profit")
   const valueResult = inputValue * 0.1
-  console.log(valueResult)
   salesProfit.innerHTML = (Math.floor(inputValue - valueResult));
-   console.log(salesProfit);
   })
 });
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -37,11 +37,23 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Category can't be blank", 'Category is not a number')
       end
 
+      it '商品のカテゴリー情報は空であるプルダウンの１つ目を選択しては出品ができない' do
+        @item.category_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category must be other than 1")
+      end
+
       it '商品の状態についての情報が空では出品できない' do
         @item.sales_status_id = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Sales status can't be blank", 'Sales status is not a number')
       end
+
+      it '商品の状態についての情報は、空であるプルダウンの１つ目を選択しては出品ができない' do
+        @item.sales_status_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Sales status must be other than 1")
+      end      
 
       it '商品の配送料の負担についての情報が空では出品できない' do
         @item.shipping_fee_status_id = ''
@@ -49,11 +61,23 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Shipping fee status can't be blank", 'Shipping fee status is not a number')
       end
 
+      it '商品の配送料の負担についての情報は、空であるプルダウンの１つ目を選択しては出品ができない' do
+        @item.shipping_fee_status_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping fee status must be other than 1")
+      end            
+
       it '商品の発送元の地域についての情報が空では出品できない' do
         @item.prefecture_id = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Prefecture can't be blank", 'Prefecture is not a number')
       end
+
+      it '商品の配送料の負担についての情報は、空であるプルダウンの１つ目を選択しては出品ができない' do
+        @item.prefecture_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture must be other than 1")
+      end                 
 
       it '商品の発送までの日数についての情報が空では出品できない' do
         @item.scheduled_delivery_id = ''
@@ -61,23 +85,35 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Scheduled delivery can't be blank", 'Scheduled delivery is not a number')
       end
 
+      it '商品の発送までの日数についての情報は、空であるプルダウンの１つ目を選択しては出品ができない' do
+        @item.scheduled_delivery_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Scheduled delivery must be other than 1")
+      end                
+      
       it '商品の価格についての情報が空では出品できない' do
         @item.price = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Price can't be blank", 'Price is not a number', 'Price Price is not a number')
       end
-
-      it '商品の価格範囲が、¥300~¥9,999,999の間以外では出品できない' do
+      
+      it '商品の価格が299円以下では出品できない' do
         @item.price = '200'
         @item.valid?
-        expect(@item.errors.full_messages).to include('Price must be greater than or equal to 300')
+        expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
+      end
+      
+      it '商品の価格が10,000,000円以上では出品できない' do
+        @item.price = '10000100'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be less than or equal to 10000000")
       end
 
       it '商品の販売価格は全角数字では保存も出品もできない' do
         @item.price = '１２３'
         @item.valid?
         expect(@item.errors.full_messages).to include('Price is not a number')
-      end
+      end        
     end
   end
 end
