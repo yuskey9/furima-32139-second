69da7e5232e0484e93b616fafa3587a4c862c4bc
yuskey9 <yuskey0130@gmail.com>
2020-12-12 21:54:42 +0900

    コードレビューによる修正完了(2回目

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -98,13 +98,13 @@ RSpec.describe Item, type: :model do
       end
       
       it '商品の価格が299円以下では出品できない' do
-        @item.price = '200'
+        @item.price = 200
         @item.valid?
         expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
       end
       
       it '商品の価格が10,000,000円以上では出品できない' do
-        @item.price = '10000100'
+        @item.price = 10000100
         @item.valid?
         expect(@item.errors.full_messages).to include("Price must be less than or equal to 10000000")
       end
