11a0ec04e142042f886e178e9dbba74edafd2cd7
yuskey9 <yuskey0130@gmail.com>
2020-12-12 18:23:14 +0900

    修正完了

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -25,5 +25,5 @@ end
 private
 
 def item_params
-  params.require(:item).permit(:name, :info, :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, :price).merge(user_id: current_user.id)
-end
+  params.require(:item).permit(:image, :name, :info, :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, :price).merge(user_id: current_user.id)
+end 
