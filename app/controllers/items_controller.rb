2852dfed257be89e8fc13031238d23d5b3914abc
yuskey9 <yuskey0130@gmail.com>
2020-12-15 21:40:25 +0900

    編集画面からの適切な画面への条件分岐の設定と、更新情報の反映実装完了

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,7 +24,19 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
-  end  
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
+    end  
+  end
+  
+  
+
   private
 
   def item_params
