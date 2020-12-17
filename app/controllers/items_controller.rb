aa34787106d9220e06a8e81bf9f9271398cd577d
yuskey9 <yuskey0130@gmail.com>
2020-12-17 16:37:12 +0900

    コードレビューによる修正完了（４回目

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, except: [:index, :new, :create]
   before_action :set_unless, only: [:edit, :update, :destroy]
 
