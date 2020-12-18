c7f87c0955c4fcebd99bf9100ca87ae8ebd848c3
yuskey9 <yuskey0130@gmail.com>
2020-12-18 16:51:41 +0900

    詳細画面から購入画面の情報の反映実装完了

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,5 +2,9 @@ class OrdersController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
     @order = Order.new
+  end
+
+
+  def create
   end  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root to: "items#index"
 
   resources :items, except: :index do
-    resources :orders, only: :index
+    resources :orders, only: [:index, :create]
   end
 end
