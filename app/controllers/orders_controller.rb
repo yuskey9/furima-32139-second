54095eaa159fb5879ef9a9984803b347465b595d
yuskey9 <yuskey0130@gmail.com>
2020-12-22 17:01:21 +0900

    エラー文の表示完了

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,10 +1,26 @@
 class OrdersController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
-    @order = Order.new
+    @order = Orderer.new
   end
 
 
   def create
+    @order = Orderer.new(order_params)
+    @item = Item.find(params[:item_id])
+    if @order.valid?
+      @order.save
+      redirect_to root_path
+    else
+      render :index
+    end  
   end  
+
+  private
+
+  def order_params
+    params.require(:orderer).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
+
 end
