73c46c4f3be1ec4d123b9eb3c90b37a6d239c37d
yuskey9 <yuskey0130@gmail.com>
2020-12-22 20:32:20 +0900

    テストコードの実装完了

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -3,7 +3,7 @@ window.addEventListener('load', () => {
   const priceInput = document.getElementById("item-price");
   priceInput.addEventListener("input", () => {
     const inputValue = priceInput.value;
-　　
+
   const addTaxDom = document.getElementById("add-tax-price");
   addTaxDom.innerHTML = (Math.floor(inputValue * 0.1));
 
--- spec/factories/orderers.rb
+++ spec/factories/orderers.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :orderer do
-    information { "MyString" }
+    postal_code          {"123-4567"}
+    prefecture_id        { 2 }
+    city                 { "名古屋市〇〇区" }
+    addresses            { "番地2-1-2" }
+    phone_number         {"09012345678"}
+
+    association :user
+    association :item
   end
 end
